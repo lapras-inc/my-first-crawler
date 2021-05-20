e9189c7260b44029c21c3fe315d8959b187717cc
kazuki.morozumi <kazuki.m777@gmail.com>
2021-05-20 18:30:05 +0900

    パーサーを強くした

--- my_first_crawler/tests/website_crawler/test_parser.py
+++ my_first_crawler/tests/website_crawler/test_parser.py
@@ -7,11 +7,22 @@ class TestParser(TestCase):
     def test(self):
         parser = Parser()
 
-        result = parser.parse(
-            '<html><head><title>LAPRASモブプロ</title><description>生配信スタート</description></head><body></body><html>'
-        )
+        result = parser.parse('''
+        <html>
+            <head>
+              <title>LAPRASモブプロ</title>
+              <meta name="description" content="生配信スタート"></meta>
+            </head>
+            <body>
+              <a href="https://lapras.com">LAPRAS</a>
+              <a href="hoge/fuga">LAPRAS</a>
+              <a href="//example.com">LAPRAS</a>
+            </body>
+        </html>
+        ''')
 
-        self.assertEqual(result, {
-            "title": "LAPRASモブプロ",
-            "description": "生配信スタート"
-        })
+        self.assertEqual(result.title, 'LAPRASモブプロ')
+        self.assertEqual(result.description, '生配信スタート')
+        self.assertIn('https://lapras.com', result.link_list)
+        self.assertIn('https://test.lapras.com/hoge/fuga', result.link_list)
+        self.assertIn('https://example.com', result.link_list)
--- my_first_crawler/website_crawler/parser.py
+++ my_first_crawler/website_crawler/parser.py
@@ -1,13 +1,27 @@
+from dataclasses import dataclass
+from typing import List
+
 from bs4 import BeautifulSoup
+
+
+@dataclass(frozen=True)
+class WebPage:
+    title: str
+    description: str
+    # ページ内に含まれるリンクURL
+    link_list: List[str]
+
 
 class Parser:
 
-    def parse(self, html):
+    def parse(self, html: str) -> WebPage:
         soup = BeautifulSoup(html, 'html.parser')
         title = soup.select_one('title').get_text()
-        description = soup.select_one('description').get_text()
+        description = soup.select_one('meta[name="description"]')['content']
+        link_list = [a['href'] for a in soup.select('a')]
 
-        return {
-            "title": title,
-            "description": description
-        }
+        return WebPage(
+            title=title,
+            description=description,
+            link_list=link_list,
+        )
