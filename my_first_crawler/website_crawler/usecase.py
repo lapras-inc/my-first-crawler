b2a0fefa860a4f8541ae69e4675a01503706cf36
kawamataryou <ba068082@gmail.com>
2021-05-27 18:56:21 +0900

    なにもわからんです

--- my_first_crawler/website_crawler/usecase.py
+++ my_first_crawler/website_crawler/usecase.py
@@ -1,6 +1,10 @@
-from website_crawler.parser import Parser
+from typing import List
+from urllib.parse import urlparse
+
+from website_crawler.parser import Parser, WebPage
 from website_crawler.scraper import Scraper
 
+crawled_url = []
 
 def crawl(seed_url: str):
     # TODO: seed_url を scrape する
@@ -8,15 +12,44 @@ def crawl(seed_url: str):
     # TODO: 戻りの WebPage.link_list の URL を再帰的に scrape する
     # TODO: クロール対象の URL がすでにクロール済みだったらスキップする
     # TODO: 同じドメインの URL だったら scrapeする、違ったらスキップする (外に出ていかなようにする)
-    crawled_url = []
+    print(web_page)
     scraper = Scraper(Parser())
     web_page = scraper.get_page(seed_url)
     crawled_url.append(seed_url)
-    print(web_page)
+    seed_domain = urlparse(seed_url).netloc
 
     for link in web_page.link_list:
-        if link in crawled_url:
+        link_domain = urlparse(link).netloc
+
+        # 同じドメインの URL だったら scrapeする、違ったらスキップする (外に出ていかなようにする)
+        if link_domain != seed_domain:
+            print('skip_url:', link)
             continue
+
+        if link in crawled_url:
+            print('skip_url:', link)
+            continue
+
+        print('get_page', link)
+        web_page = scraper.get_page(link)
+        crawled_url.append(link)
+        print(web_page)
+
+def xxx(web_page: WebPage, seed_domain: str, crawled_url: List[str]) -> List[WebPage]:
+    scraper = Scraper(Parser())
+
+    for link in web_page.link_list:
+        link_domain = urlparse(link).netloc
+
+        # 同じドメインの URL だったら scrapeする、違ったらスキップする (外に出ていかなようにする)
+        if link_domain != seed_domain:
+            print('skip_url:', link)
+            continue
+
+        if link in crawled_url:
+            print('skip_url:', link)
+            continue
+
         print('get_page', link)
         web_page = scraper.get_page(link)
         crawled_url.append(link)
